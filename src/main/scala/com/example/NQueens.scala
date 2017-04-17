eaf9f3f88ca230a28029b731ee3a1a4e0f991ab4
Florent Le Gall <florent.legall@gmail.com>
2017-04-17 12:45:32 +0200

    Add naive brute-force solution

--- src/main/scala/com/example/NQueens.scala
+++ src/main/scala/com/example/NQueens.scala
@@ -1,13 +1,72 @@
 package com.example
 
-case class Queen(x: Int, y: Int)
+import scala.util.Sorting
+
+case class Queen(x: Int, y: Int) extends Ordered[Queen] {
+  def canSee(queen: Queen): Boolean = {
+    queen.x == this.x || queen.y == this.y ||
+    queen.x - queen.y == this.x - this.y ||
+    queen.x + queen.y == this.x + this.y
+  }
+
+  def canInsertInto(queens: Seq[Queen]): Boolean = {
+    !queens.exists(q => q.canSee(this))
+  }
+
+  import scala.math.Ordered.orderingToOrdered
+  def compare(that: Queen): Int =
+    (this.x, this.y) compare (that.x, that.y)
+}
 
 object NQueens {
   def main(args: Array[String]): Unit = {
-    println(solve(8))
+    var solutions: Set[Seq[Queen]] = Set()
+
+    solve((solution: Seq[Queen]) => {
+      if (!solutions.contains(solution)) {
+        solutions = solutions + solution
+        printSolution(solution)
+      }
+    })
   }
 
-  def solve(n: Int): Seq[Queen] = {
-    Seq()
+  private def solve(onFound: (Seq[Queen]) => Unit,
+                    level: Int = 0,
+                    queens: Seq[Queen] = Seq()): Unit = {
+    if (level < 8) {
+      for (x <- 0 until 8) {
+        for (y <- 0 until 8) {
+          val queen = Queen(x, y)
+          if (queen.canInsertInto(queens)) {
+            val updatedQueens = queens :+ queen
+            solve(onFound, level + 1, updatedQueens)
+          }
+        }
+      }
+    } else {
+      onFound(Sorting.stableSort(queens))
+    }
+  }
+
+  private def printSolution(solution: Seq[Queen]) = {
+    val grid = solution.to[Set]
+
+    println("----------")
+
+    for (y <- 0 until 8) {
+      print('|')
+
+      for (x <- 0 until 8) {
+        if (grid.contains(Queen(x, y))) {
+          print('x')
+        } else {
+          print(' ')
+        }
+      }
+
+      println('|')
+    }
+
+    println("----------")
   }
 }
---
+++ src/test/scala/com/example/NQueensSpec/NQueensSpec.scala
@@ -0,0 +1,50 @@
+package com.example.NQueensSpec
+
+import com.example.Queen
+import org.scalatest.{FunSpec, Matchers}
+
+class NQueensSpec extends FunSpec with Matchers {
+  describe("A Queen") {
+    it("can see another queen at the same position") {
+      val aQueen = Queen(1, 2)
+      val anotherQueen = Queen(1, 2)
+
+      aQueen.canSee(anotherQueen) shouldBe true
+    }
+
+    it("can see another queen on the same column") {
+      val aQueen = Queen(1, 2)
+      val anotherQueen = Queen(1, 3)
+
+      aQueen.canSee(anotherQueen) shouldBe true
+    }
+
+    it("can see another queen on the same row") {
+      val aQueen = Queen(1, 2)
+      val anotherQueen = Queen(3, 2)
+
+      aQueen.canSee(anotherQueen) shouldBe true
+    }
+
+    it("cannot see another queen on a different position") {
+      val aQueen = Queen(1, 1)
+      val anotherQueen = Queen(3, 2)
+
+      aQueen.canSee(anotherQueen) shouldBe false
+    }
+
+    it("can see another queen on the same growing diagonal") {
+      val aQueen = Queen(1, 2)
+      val anotherQueen = Queen(2, 3)
+
+      aQueen.canSee(anotherQueen) shouldBe true
+    }
+
+    it("can see another queen on the same ungrowing diagonal") {
+      val aQueen = Queen(1, 0)
+      val anotherQueen = Queen(0, 1)
+
+      aQueen.canSee(anotherQueen) shouldBe true
+    }
+  }
+}
