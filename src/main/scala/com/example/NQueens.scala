0512b59f70e5cc4863be01e7dbf2b5d859fb05da
Florent Le Gall <florent.legall@gmail.com>
2017-04-17 15:04:33 +0200

    Improve solution, by forcing y to be the level and enforcing the "only one queen by row rule"

--- src/main/scala/com/example/NQueens.scala
+++ src/main/scala/com/example/NQueens.scala
@@ -1,35 +1,27 @@
 package com.example
 
-import scala.util.Sorting
-
-case class Queen(x: Int, y: Int) extends Ordered[Queen] {
-  def canSee(queen: Queen): Boolean = {
-    queen.x == this.x || queen.y == this.y ||
-    queen.x - queen.y == this.x - this.y ||
-    queen.x + queen.y == this.x + this.y
+case class Queen(x: Int, y: Int) {
+  def canAttack(anotherQueen: Queen): Boolean = {
+    anotherQueen.x == this.x || anotherQueen.y == this.y ||
+    anotherQueen.x - anotherQueen.y == this.x - this.y ||
+    anotherQueen.x + anotherQueen.y == this.x + this.y
   }
 
   def canInsertInto(queens: Seq[Queen]): Boolean = {
-    !queens.exists(q => q.canSee(this))
+    !queens.exists(q => q.canAttack(this))
   }
-
-  import scala.math.Ordered.orderingToOrdered
-  def compare(that: Queen): Int =
-    (this.x, this.y) compare (that.x, that.y)
 }
 
 object NQueens {
   def main(args: Array[String]): Unit = {
     val N = 8
-    var solutions: Set[Seq[Queen]] = Set()
+    var solutions: Seq[Seq[Queen]] = Seq()
 
     solve(N, (solution: Seq[Queen]) => {
-      if (!solutions.contains(solution)) {
-        solutions = solutions + solution
-        printSolution(N, solution)
-      }
+      solutions = solutions :+ solution
     })
 
+    solutions.foreach(solution => printSolution(N, solution))
     println(s"${solutions.size} solutions found")
   }
 
@@ -39,23 +31,20 @@ object NQueens {
                     queens: Seq[Queen] = Seq()): Unit = {
     if (level < n) {
       for (x <- 0 until n) {
-        for (y <- 0 until n) {
-          val queen = Queen(x, y)
-          if (queen.canInsertInto(queens)) {
-            val updatedQueens = queens :+ queen
-            solve(n, onFound, level + 1, updatedQueens)
-          }
+        val queen = Queen(x, level)
+        if (queen.canInsertInto(queens)) {
+          solve(n, onFound, level + 1, queens :+ queen)
         }
       }
     } else {
-      onFound(Sorting.stableSort(queens))
+      onFound(queens)
     }
   }
 
   private def printSolution(n: Int, solution: Seq[Queen]) = {
     val grid = solution.to[Set]
 
-    println("----------")
+    println("-" * (n + 2))
 
     for (y <- 0 until n) {
       print('|')
@@ -71,6 +60,6 @@ object NQueens {
       println('|')
     }
 
-    println("----------")
+    println("-" * (n + 2))
   }
 }
--- src/test/scala/com/example/NQueensSpec/NQueensSpec.scala
+++ src/test/scala/com/example/NQueensSpec/NQueensSpec.scala
@@ -9,42 +9,42 @@ class NQueensSpec extends FunSpec with Matchers {
       val aQueen = Queen(1, 2)
       val anotherQueen = Queen(1, 2)
 
-      aQueen.canSee(anotherQueen) shouldBe true
+      aQueen.canAttack(anotherQueen) shouldBe true
     }
 
     it("can see another queen on the same column") {
       val aQueen = Queen(1, 2)
       val anotherQueen = Queen(1, 3)
 
-      aQueen.canSee(anotherQueen) shouldBe true
+      aQueen.canAttack(anotherQueen) shouldBe true
     }
 
     it("can see another queen on the same row") {
       val aQueen = Queen(1, 2)
       val anotherQueen = Queen(3, 2)
 
-      aQueen.canSee(anotherQueen) shouldBe true
+      aQueen.canAttack(anotherQueen) shouldBe true
     }
 
     it("cannot see another queen on a different position") {
       val aQueen = Queen(1, 1)
       val anotherQueen = Queen(3, 2)
 
-      aQueen.canSee(anotherQueen) shouldBe false
+      aQueen.canAttack(anotherQueen) shouldBe false
     }
 
     it("can see another queen on the same growing diagonal") {
       val aQueen = Queen(1, 2)
       val anotherQueen = Queen(2, 3)
 
-      aQueen.canSee(anotherQueen) shouldBe true
+      aQueen.canAttack(anotherQueen) shouldBe true
     }
 
     it("can see another queen on the same ungrowing diagonal") {
       val aQueen = Queen(1, 0)
       val anotherQueen = Queen(0, 1)
 
-      aQueen.canSee(anotherQueen) shouldBe true
+      aQueen.canAttack(anotherQueen) shouldBe true
     }
   }
 }
