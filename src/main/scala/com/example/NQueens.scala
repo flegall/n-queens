2b4895993309d0208350185fb5457c3900d6f688
Florent Le Gall <florent.legall@gmail.com>
2017-04-17 13:13:19 +0200

    Make N a parameter

--- src/main/scala/com/example/NQueens.scala
+++ src/main/scala/com/example/NQueens.scala
@@ -20,26 +20,30 @@ case class Queen(x: Int, y: Int) extends Ordered[Queen] {
 
 object NQueens {
   def main(args: Array[String]): Unit = {
+    val N = 8
     var solutions: Set[Seq[Queen]] = Set()
 
-    solve((solution: Seq[Queen]) => {
+    solve(N, (solution: Seq[Queen]) => {
       if (!solutions.contains(solution)) {
         solutions = solutions + solution
-        printSolution(solution)
+        printSolution(N, solution)
       }
     })
+
+    println(s"${solutions.size} solutions found")
   }
 
-  private def solve(onFound: (Seq[Queen]) => Unit,
+  private def solve(n: Int,
+                    onFound: (Seq[Queen]) => Unit,
                     level: Int = 0,
                     queens: Seq[Queen] = Seq()): Unit = {
-    if (level < 8) {
-      for (x <- 0 until 8) {
-        for (y <- 0 until 8) {
+    if (level < n) {
+      for (x <- 0 until n) {
+        for (y <- 0 until n) {
           val queen = Queen(x, y)
           if (queen.canInsertInto(queens)) {
             val updatedQueens = queens :+ queen
-            solve(onFound, level + 1, updatedQueens)
+            solve(n, onFound, level + 1, updatedQueens)
           }
         }
       }
@@ -48,15 +52,15 @@ object NQueens {
     }
   }
 
-  private def printSolution(solution: Seq[Queen]) = {
+  private def printSolution(n: Int, solution: Seq[Queen]) = {
     val grid = solution.to[Set]
 
     println("----------")
 
-    for (y <- 0 until 8) {
+    for (y <- 0 until n) {
       print('|')
 
-      for (x <- 0 until 8) {
+      for (x <- 0 until n) {
         if (grid.contains(Queen(x, y))) {
           print('x')
         } else {
